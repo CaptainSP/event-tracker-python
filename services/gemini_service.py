5cb46fdfa647df73b98697c891a26cb84e632e7b
CaptainSP <19982002ke@gmail.com>
2024-12-19 07:54:38 +0300

    Update EventExtractorResponse to make eventData optional

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -30,7 +30,7 @@ class EventData(typing.TypedDict):
 
 class EventExtractorResponse(typing.TypedDict):
     hasEvent: bool
-    eventData: dict[str,EventData]
+    eventData: typing.Optional[EventData]
     
 
 class EventExtractor:
